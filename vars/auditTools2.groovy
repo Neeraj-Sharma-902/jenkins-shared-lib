c1fb7bac335d335ce480e972d4ec851559061107
Neeraj Sharma <sneeraj62@gmail.com>
2024-01-16 04:38:46 +0530

    auditTools2

--- vars/auditTools2.groovy
+++ vars/auditTools2.groovy
@@ -1,7 +1,7 @@
 def call(Map config){
     node{
         sh """
-            echo "Config message is : ${config.message}
+            echo "Config message is : ${config.message}"
             git version
             java -version
             mvn -version
